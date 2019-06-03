4e6385e3e83563a4d9735406112ded2fbb5bdcb9
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-06-03 17:47:16 +0200

    apb_intf: Add clocked interface for design verification

--- src/apb_intf.sv
+++ src/apb_intf.sv
@@ -43,3 +43,40 @@ interface APB;
   );
 
 endinterface
+
+// A clocked APB2 interface for use in design verification
+interface APB_DV (
+  input logic clk_i
+);
+
+  localparam ADDR_WIDTH = 32;
+  localparam DATA_WIDTH = 32;
+  localparam STRB_WIDTH = DATA_WIDTH / 8;
+
+  typedef logic [ADDR_WIDTH-1:0]  addr_t;
+  typedef logic [DATA_WIDTH-1:0]  data_t;
+  typedef logic            [2:0]  prot_t;
+  typedef logic [STRB_WIDTH-1:0]  strb_t;
+
+  addr_t  paddr;
+  prot_t  pprot;
+  logic   psel;
+  logic   penable;
+  logic   pwrite;
+  data_t  pwdata;
+  strb_t  pstrb;
+  logic   pready;
+  data_t  prdata;
+  logic   pslverr;
+
+  modport Master (
+    output paddr, pprot, psel, penable, pwrite, pwdata, pstrb,
+    input  pready, prdata, pslverr
+  );
+
+  modport Slave (
+    input  paddr, pprot, psel, penable, pwrite, pwdata, pstrb,
+    output pready, prdata, pslverr
+  );
+
+endinterface
