c1757d6ed8db56da58e0ec07463ec509b597cc54
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-06-30 09:17:04 +0200

    apb_ro_regs: Make address and data width parametrizable

--- src/apb_ro_regs.sv
+++ src/apb_ro_regs.sv
@@ -12,26 +12,32 @@
 // This module exposes a number of 32-bit registers (provided on the `reg_i` input) read-only on an
 // APB interface.  It responds to reads that are out of range and writes with a slave error.
 module apb_ro_regs #(
-  parameter int unsigned N_REGS = 0
+  parameter int unsigned N_REGS = 0,
+  parameter int unsigned ADDR_WIDTH = 0,
+  parameter int unsigned DATA_WIDTH = 0,
+  localparam type addr_t = logic[ADDR_WIDTH-1:0],
+  localparam type data_t = logic[DATA_WIDTH-1:0],
+  localparam type strb_t = logic[DATA_WIDTH/8-1:0]
 ) (
   // APB Interface
   input  logic        pclk_i,
   input  logic        preset_ni,
-  input  logic [31:0] paddr_i,
-  input  logic  [2:0] pprot_i,
+  input  addr_t       paddr_i,
+  input  logic [2:0]  pprot_i,
   input  logic        psel_i,
   input  logic        penable_i,
   input  logic        pwrite_i,
-  input  logic [31:0] pwdata_i,
-  input  logic  [3:0] pstrb_i,
+  input  data_t       pwdata_i,
+  input  strb_t       pstrb_i,
   output logic        pready_o,
-  output logic [31:0] prdata_o,
+  output data_t       prdata_o,
   output logic        pslverr_o,
 
   // Register Interface
-  input  logic [N_REGS-1:0][31:0] reg_i
+  input  data_t [N_REGS-1:0] reg_i
 );
 
+  localparam int unsigned WORD_OFFSET = $clog2(DATA_WIDTH/8);
   always_comb begin
     prdata_o  = 'x;
     pslverr_o = 1'b0;
@@ -40,7 +46,7 @@ module apb_ro_regs #(
         // Error response to writes
         pslverr_o = 1'b1;
       end else begin
-        automatic logic [29:0] word_addr = paddr_i >> 2;
+        automatic logic [ADDR_WIDTH-WORD_OFFSET-1:0] word_addr = paddr_i >> WORD_OFFSET;
         if (word_addr >= N_REGS) begin
           // Error response to reads out of range
           pslverr_o = 1'b1;
