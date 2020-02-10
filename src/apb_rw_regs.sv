c94d1f92e3d40052dcf9e563ee2e23bea5ecc67a
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-02-10 17:21:13 +0100

    apb_rw_regs: Add variable address and data width

--- src/apb_rw_regs.sv
+++ src/apb_rw_regs.sv
@@ -9,48 +9,62 @@
 // specific language governing permissions and limitations under the License.
 
 // APB Read-Write Registers
-// This module exposes a number of 32-bit registers (provided on the `reg_i` input) writable on an
+// This module exposes a number of registers (provided on the `reg_i` input) writable on an
 // APB interface.  It responds to accesses that are out of range with a slave error.
+`include "common_cells/registers.svh"
+
 module apb_rw_regs #(
-  parameter int unsigned N_REGS = 0
+  parameter int unsigned NoRegs = 0
+  parameter int unsigned AddrWidth = 32'd0,
+  parameter int unsigned DataWidth = 32'd0,
+  // DEPENDENT PARAMETERS DO NOT OVERWRITE!
+  parameter int unsigned StrbWidth = cf_math_pkg::ceil_div(DataWidth, 8),
+  parameter type         addr_t    = logic[AddrWidth-1:0],
+  parameter type         data_t    = logic[DataWidth-1:0],
+  parameter type         strb_t    = logic[StrbWidth-1:0]
 ) (
   // APB Interface
-  input  logic        pclk_i,
-  input  logic        preset_ni,
-  input  logic [31:0] paddr_i,
-  input  logic  [2:0] pprot_i,
-  input  logic        psel_i,
-  input  logic        penable_i,
-  input  logic        pwrite_i,
-  input  logic [31:0] pwdata_i,
-  input  logic  [3:0] pstrb_i,
-  output logic        pready_o,
-  output logic [31:0] prdata_o,
-  output logic        pslverr_o,
+  input  logic           pclk_i,
+  input  logic           preset_ni,
+  input  addr_t          paddr_i,
+  input  apb_pkg::prot_t pprot_i,
+  input  logic           psel_i,
+  input  logic           penable_i,
+  input  logic           pwrite_i,
+  input  data_t          pwdata_i,
+  input  strb_t          pstrb_i,
+  output logic           pready_o,
+  output data_t          prdata_o,
+  output logic           pslverr_o,
 
   // Register Interface
-  input  logic [N_REGS-1:0][31:0] init_i,
-  output logic [N_REGS-1:0][31:0] q_o
+  input  data_t [NoRegs-1:0] init_i,
+  output data_t [NoRegs-1:0] q_o
 );
+  // if StrbWidth = 1 we want Word Offset = 0
+  localparam int unsigned WordOffset = $clog2(StrbWidth);
 
-  logic [N_REGS-1:0][31:0] reg_d, reg_q;
+  data_t [NoRegs-1:0] reg_d,      reg_q;
+  logic               reg_update;
 
   always_comb begin
-    reg_d     = reg_q;
-    prdata_o  = 'x;
-    pslverr_o = 1'b0;
+    reg_d      = reg_q;
+    reg_update = 1'b0;
+    prdata_o   = data_t'(32'h0BAD_B10C);
+    pslverr_o  = apb_pkg::RESP_OKAY;
     if (psel_i) begin
-      automatic logic [29:0] word_addr = paddr_i >> 2;
-      if (word_addr >= N_REGS) begin
+      automatic logic [AddrWidth-WordOffset-1:0] word_addr = paddr_i >> WordOffset;
+      if (word_addr >= NoRegs) begin
         // Error response to accesses that are out of range
-        pslverr_o = 1'b1;
+        pslverr_o = apb_pkg::RESP_SLVERR;
       end else begin
         if (pwrite_i) begin
-          for (int i = 0; i < 4; i++) begin
-            if (pstrb_i[i]) begin
-              reg_d[word_addr][i*8 +: 8] = pwdata_i[i*8 +: 8];
+          for (int unsigned i = 0; i < DataWidth; i++) begin
+            if (pstrb_i[i/8]) begin
+              reg_d[word_addr][i] = pwdata_i[i];
             end
           end
+          reg_update = |pstrb_i; // only update register when a write strobe is set
         end else begin
           prdata_o = reg_q[word_addr];
         end
@@ -61,19 +75,15 @@ module apb_rw_regs #(
 
   assign q_o = reg_q;
 
-  always_ff @(posedge pclk_i or negedge preset_ni) begin
-    if (!preset_ni) begin
-      reg_q <= init_i;
-    end else begin
-      reg_q <= reg_d;
-    end
-  end
+  `FFLARN(reg_q, reg_d, reg_update, init_i, pclk_i, preset_ni)
 
 // Validate parameters.
 // pragma translate_off
 `ifndef VERILATOR
   initial begin: p_assertions
-    assert (N_REGS >= 1) else $fatal(1, "The number of registers must be at least 1!");
+    assert (NoRegs    > 0)          else $fatal(1, "The number of registers must be at least 1!");
+    assert (AddrWidth > WordOffset) else $fatal(1, "AddrWidth is not wide enough!");
+    assert (DataWidth > 0)          else $fatal(1, "DataWidth has to be > 0!");
   end
 `endif
 // pragma translate_on
