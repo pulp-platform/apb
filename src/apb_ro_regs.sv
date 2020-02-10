39cf51143f941c6ef56394d32aa509cc6dd47066
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-02-10 17:19:59 +0100

    apb_ro_regs: Add variable address and data width

--- src/apb_ro_regs.sv
+++ src/apb_ro_regs.sv
@@ -9,41 +9,50 @@
 // specific language governing permissions and limitations under the License.
 
 // APB Read-Only Registers
-// This module exposes a number of 32-bit registers (provided on the `reg_i` input) read-only on an
+// This module exposes a number of registers (provided on the `reg_i` input) read-only on an
 // APB interface.  It responds to reads that are out of range and writes with a slave error.
 module apb_ro_regs #(
-  parameter int unsigned N_REGS = 0
+  parameter int unsigned NoRegs    = 32'd0,
+  parameter int unsigned AddrWidth = 32'd0,
+  parameter int unsigned DataWidth = 32'd0,
+  // DEPENDENT PARAMETERS DO NOT OVERWRITE!
+  parameter int unsigned StrbWidth = cf_math_pkg::ceil_div(DataWidth, 8),
+  parameter type         addr_t    = logic[AddrWidth-1:0],
+  parameter type         data_t    = logic[DataWidth-1:0],
+  parameter type         strb_t    = logic[StrbWidth-1:0]
 ) (
   // APB Interface
-  input  logic        pclk_i,
-  input  logic        preset_ni,
-  input  logic [31:0] paddr_i,
-  input  logic  [2:0] pprot_i,
-  input  logic        psel_i,
-  input  logic        penable_i,
-  input  logic        pwrite_i,
-  input  logic [31:0] pwdata_i,
-  input  logic  [3:0] pstrb_i,
-  output logic        pready_o,
-  output logic [31:0] prdata_o,
-  output logic        pslverr_o,
+  input  logic           pclk_i,
+  input  logic           preset_ni,
+  input  addr_t          paddr_i,
+  input  apb_pkg::prot_t pprot_i,
+  input  logic           psel_i,
+  input  logic           penable_i,
+  input  logic           pwrite_i,
+  input  data_t          pwdata_i,
+  input  strb_t          pstrb_i,
+  output logic           pready_o,
+  output data_t          prdata_o,
+  output logic           pslverr_o,
 
   // Register Interface
-  input  logic [N_REGS-1:0][31:0] reg_i
+  input  data_t [NoRegs-1:0] reg_i
 );
+  // if StrbWidth = 1 we want Word Offset = 0
+  localparam int unsigned WordOffset = $clog2(StrbWidth);
 
   always_comb begin
-    prdata_o  = 'x;
-    pslverr_o = 1'b0;
+    prdata_o  = data_t'(32'h0BAD_B10C);
+    pslverr_o = apb_pkg::RESP_OKAY;
     if (psel_i) begin
       if (pwrite_i) begin
         // Error response to writes
-        pslverr_o = 1'b1;
+        pslverr_o = apb_pkg::RESP_SLVERR;
       end else begin
-        automatic logic [29:0] word_addr = paddr_i >> 2;
-        if (word_addr >= N_REGS) begin
+        automatic logic [AddrWidth-WordOffset-1:0] word_addr = paddr_i >> WordOffset;
+        if (word_addr >= NoRegs) begin
           // Error response to reads out of range
-          pslverr_o = 1'b1;
+          pslverr_o = apb_pkg::RESP_SLVERR;
         end else begin
           prdata_o = reg_i[word_addr];
         end
@@ -56,7 +65,9 @@ module apb_ro_regs #(
 // pragma translate_off
 `ifndef VERILATOR
   initial begin: p_assertions
-    assert (N_REGS >= 1) else $fatal(1, "The number of registers must be at least 1!");
+    assert (NoRegs    > 0)          else $fatal(1, "The number of registers must be at least 1!");
+    assert (AddrWidth > WordOffset) else $fatal(1, "AddrWidth is not wide enough!");
+    assert (DataWidth > 0)          else $fatal(1, "DataWidth has to be > 0!");
   end
 `endif
 // pragma translate_on
